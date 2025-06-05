b2bcab1f1e8bd3cf73ba4a59a897ed20f4eebc22
sidra976 <sidraahmed4477@gmail.com>
2025-06-05 14:36:10 +0500

    add files

--- php-basic/one.php
+++ php-basic/one.php
@@ -3,4 +3,16 @@ echo "PHP with Html and Css";
 echo "<br>";
 echo "<h1> HTML Heading </h1>";
 echo "<h1 style ='color:red'> HTML Heading with styling </h1>";
+echo "<br>";
+echo "Using variables";
+$city = "Newyork";
+echo "<h2>$city</h2>";
+echo "<h2>City: $city</h2>";
+
+$colorName = "green";
+$name = "Sara";
 ?>
+
+<h1 style = "color:  <?php echo $colorName;?>">
+    <?php echo "name: $name"?>
+</h1>
